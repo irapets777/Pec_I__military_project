f93e7084b8147bf9ddd1140e49c8fe33f41dfbb7
irapets777 <irapets777@gmail.com>
2022-12-02 00:28:08 +0200

    hero

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,15 @@ let slider = $(document).ready(function () {
   });
 });
 
+$('.header__burger').on('click', function(){
+  $('.header__burger').toggleClass('active');
+  // $('.mobile__nav').toggleClass('active');
+})
+
+$('.mobile__nav a').on('click', function(){
+  $('.header__burger').toggleClass('active');
+  $('.mobile__nav').toggleClass('active');
+})
+
+
+
